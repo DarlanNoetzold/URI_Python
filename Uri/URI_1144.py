45a04378ffe8b00c0662e102d8430c9d3d26cf0c
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2021-01-07 08:29:12 -0300

    Create URI_1144.py

---
+++ Uri/URI_1144.py
@@ -0,0 +1,8 @@
+num = int(input())
+
+i = 1
+
+while i <= num :
+    print(str(int(i))+" "+str(int(i*i))+" "+str(int(i*i*i)))
+    print(str(int(i))+" "+str(int(i*i+1))+" "+str(int(i*i*i+1)))
+    i += 1
